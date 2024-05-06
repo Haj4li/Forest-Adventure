a336da4436f50b2f3589f557d0f8fe51f118d64f
Ali Nakhaee <alinakhaee78@gmail.com>
2024-05-06 12:40:33 +0330

    sprite sheet manager in sprite class
    Added some basic animation functionality

--- modules/sprites.py
+++ modules/sprites.py
@@ -9,8 +9,8 @@ def LoadImage(path):
     images[path] = pygame.image.load(path)
     return images[path]
 
-
 class Sprite():
+    _animations = {}
     def __init__(self, image_path, x,y,tag="NOTAGE"):
         global images
         self._imgpath = image_path
@@ -25,7 +25,40 @@ class Sprite():
         self.rect.y = y
         self.position = pygame.Vector2(x, y)
         self.tag = tag
-    
+
+        #animations
+        self.current_animation = None
+        self.current_frame_index = 0
+        self.last_frame_update = pygame.time.get_ticks()
+        self.playing_animation = False
+        self.setupSpritesheet(1,1)
+        self.current_row = 0
+
+    def setupSpritesheet(self,total_rows,total_cols):
+        self.rows = total_rows
+        self.cols = total_cols
+        self.frame_width = self.rect.width // self.cols
+        self.frame_height = self.rect.height // self.rows
+
+    def addAnimation(self, name, row, frames,speed, loop):
+        self._animations[name] = (row, frames, speed, loop)
+
+    def setAnimation(self,animation):
+        self.current_animation = animation
+        self.current_frame_index = 0
+        self.last_frame_update = pygame.time.get_ticks()
+        self.playing_animation = True
+        self.current_row = self.current_animation[0]
+
+    def playAnimation(self,name):
+        if (name in self._animations):
+            self.setAnimation(self._animations[name])
+
+
+    def stopAnimation(self):
+        self.playing_animation = False
+
+
     def rotate(self, angle):
         self.image = pygame.transform.rotate(self._image, angle)
 
@@ -37,10 +70,31 @@ class Sprite():
         pass
 
     def clone(self):
-        return type(self)(self._imgpath,self.rect.x,self.rect.y,self.tag)
-    
+        cloned = type(self)(self._imgpath,self.rect.x,self.rect.y,self.tag)
+        if (len(self._animations) > 0):
+            for anims in self._animations.keys():
+                cloned.addAnimation(anims,self._animations[anims][0],self._animations[anims][1],self._animations[anims][2],self._animations[anims][3])
+            cloned.setAnimation(self.current_animation)
+        return cloned
+
     def draw(self, surface):
-        surface.blit(self.image, self.rect)
+        # check animation state
+        if (self.playing_animation and self.current_animation != None): # play animation frames
+            current_time = pygame.time.get_ticks()
+
+            if current_time - self.last_frame_update > self.current_animation[2]:
+                self.last_frame_update = current_time
+                self.current_frame_index += 1
+
+                if self.current_frame_index >= self.current_animation[1]:
+                    if self.current_animation[3]:
+                        self.current_frame_index = 0
+                    else:
+                        self.current_frame_index = len(self.frames) - 1
+                        self.playing_animation = False
+
+        frame = self.image.subsurface(pygame.Rect(self.current_frame_index*self.frame_width, self.current_row*self.frame_height, self.frame_width,self.frame_height)).convert_alpha()
+        surface.blit(frame, self.rect)
     
     def drawAt(self, x, y,surface):
         surface.blit(self.image, pygame.Rect(x,y,self.rect.width,self.rect.height))
